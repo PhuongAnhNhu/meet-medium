1a2ad489b8ab96fac526136313abdf1c413de016
Anna Phuong Anh <phuong.nhu@endava.com>
2022-02-20 14:47:47 +0100

    Add react routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AppRoutes from './Routes';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppRoutes></AppRoutes>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
