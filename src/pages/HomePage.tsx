0871025544a7a878fe0e6d46659f27c7b09c7259
Anna Phuong Anh <phuong.nhu@endava.com>
2022-03-12 14:19:20 +0100

    Get Room List

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState, useAppDispatch } from 'store';
 import Header from '../components/Header';
+import { fetchRoomList } from '../store/features/roomSlice';
 
 const Homepage = () => {
+  const dispatch = useAppDispatch();
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+
+  useEffect(() => {
+    if (accessToken) {
+      dispatch(fetchRoomList(accessToken));
+    }
+  }, [accessToken, dispatch]);
+
   return (
     <div>
       <Header />
---
+++ src/store/features/roomSlice.ts
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getRoomList, Room } from '../../api/room';
+
+export interface RoomListState {
+  roomList?: Room[];
+  isLoading: boolean;
+  error: unknown[];
+}
+
+const initialState: RoomListState = {
+  roomList: [],
+  isLoading: false,
+  error: [],
+};
+
+export const fetchRoomList = createAsyncThunk('roomList/fetchRoomList', async (accessToken: string) => {
+  const response = await getRoomList(accessToken);
+  return response.value;
+});
+
+// getRoomList(accessToken);
+
+export const roomSlice = createSlice({
+  name: 'room',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRoomList.fulfilled, (state, action) => {
+        console.log(action.payload);
+        state.roomList = action.payload;
+      })
+      .addCase(fetchRoomList.rejected, (state, action) => {
+        state.error.push(action.payload);
+      });
+  },
+});
+
+export default roomSlice.reducer;
