f168aa422e96195732df856a734f56c5814646a6
Anna Phuong Anh <phuong.nhu@endava.com>
2022-03-17 09:16:25 +0100

    Grid in Dashboard

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,24 @@
-import {} from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState, useAppDispatch } from 'store';
 import { fetchRoomList } from '../store/features/roomSlice';
+import RoomCard from '../components/RoomCard';
+import { Room } from 'api/room';
 
 const Homepage = () => {
   const dispatch = useAppDispatch();
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
+
+  //Get RoomList
+  const roomList = useSelector((state: RootState) => state.room.roomList);
+
+  //Nur Räume in Berlin angezeigt werden
+  const roomInBerlin = roomList.filter((room: Room) => {
+    if (room.name.includes('Berlin')) {
+      return room;
+    }
+  });
 
   useEffect(() => {
     if (accessToken) {
@@ -15,11 +27,15 @@ const Homepage = () => {
   }, [accessToken, dispatch]);
 
   return (
-    //Bei offen : ml= {30}
-    //Bei Close ml={8}
-    // <Box mt={5} ml={30} component="main" sx={{ flexGrow: 1, p: 3 }}>
-    <h1>HomePage</h1>
-    // </Box>
+    <Box mt={4} mr={2}>
+      <Grid container rowSpacing={2} columnSpacing={{ sm: 2, md: 1 }}>
+        {roomInBerlin.map((room) => (
+          <Grid key={room.name} item xs={10} sm={6} md={4}>
+            <RoomCard name={room.name} address={room.address} />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
